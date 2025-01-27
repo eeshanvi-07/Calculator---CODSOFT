5ce46e78959335eb36d28848cc263dec7df01199
Mustala Eeshanvi <meeshanvirq@gmail.com>
2025-01-27 23:57:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,84 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const display = document.getElementById('display');
-    let currentInput = '';
-    let firstOperand = null;
-    let operator = null;
-    let waitingForSecondOperand = false;
+document.addEventListener("keydown", keyboardInputHandler);
 
-    function handleNumberClick(number) {
-        if (waitingForSecondOperand) {
-            currentInput = number;
-            waitingForSecondOperand = false;
-        } else {
-            currentInput += number;
-        }
-        display.value = currentInput;
-    }
+function keyboardInputHandler(e) {
+   let res = document.getElementById("result");
 
-    function handleOperatorClick(nextOperator) {
-        const inputValue = parseFloat(currentInput);
-        if (operator && waitingForSecondOperand) {
-            operator = nextOperator;
-            return;
-        }
-        if (firstOperand === null) {
-            firstOperand = inputValue;
-        } else if (operator) {
-            const result = performCalculation(firstOperand, inputValue, operator);
-            display.value = result;
-            firstOperand = result;
-        }
-        waitingForSecondOperand = true;
-        operator = nextOperator;
-    }
+   if (e.key === "0") {
+      res.value += "0";
+   } else if (e.key === "1") {
+      res.value += "1";
+   } else if (e.key === "2") {
+      res.value += "2";
+   } else if (e.key === "3") {
+      res.value += "3";
+   } else if (e.key === "4") {
+      res.value += "4";
+   } else if (e.key === "5") {
+      res.value += "5";
+   } else if (e.key === "6") {
+      res.value += "6";
+   } else if (e.key === "7") {
+      res.value += "7";
+   } else if (e.key === "8") {
+      res.value += "8";
+   } else if (e.key === "9") {
+      res.value += "9";
+   }
 
-    function performCalculation(num1, num2, operator) {
-        switch (operator) {
-            case '+':
-                return num1 + num2;
-            case '-':
-                return num1 - num2;
-            case '*':
-                return num1 * num2;
-            case '/':
-                return num1 / num2;
-            default:
-                return num2;
-        }
-    }
+   if (e.key === "+") {
+      res.value += "+";
+   } else if (e.key === "-") {
+      res.value += "-";
+   } else if (e.key === "*") {
+      res.value += "*";
+   } else if (e.key === "/") {
+      res.value += "/";
+   } else if (e.key === "%") {
+      res.value += "%";
+   }
 
-    function handleClear() {
-        currentInput = '';
-        firstOperand = null;
-        operator = null;
-        waitingForSecondOperand = false;
-        display.value = '';
-    }
+   if (e.key === ".") {
+      res.value += ".";
+   }
 
-    document.querySelectorAll('.buttons button').forEach(button => {
-        button.addEventListener('click', () => {
-            const buttonValue = button.textContent;
-            if (!isNaN(buttonValue) || buttonValue === '.') {
-                handleNumberClick(buttonValue);
-            } else if (buttonValue === 'C') {
-                handleClear();
-            } else if (buttonValue === '=') {
-                handleOperatorClick(buttonValue);
-            } else {
-                handleOperatorClick(buttonValue);
-            }
-        });
-    });
-});
+   if (e.key === "Enter") {
+      res.value = eval(result.value || null);
+   }
+
+   if (e.key === "Backspace") {
+      let resultInput = res.value;
+
+      res.value = resultInput.substring(0, res.value.length - 1);
+   }
+}
+
+function clearScreen() {
+   document.getElementById("result").value = "";
+}
+
+function liveScreen(value) {
+   let res = document.getElementById("result");
+   if (!res.value) {
+      res.value = "";
+   }
+   res.value += value;
+}
+
+function backSpace() {
+   let resultInput = document.getElementById("result").value;
+
+   document.getElementById("result").value = resultInput.substring(
+      0,
+      resultInput.length - 1
+   );
+}
+
+function changeTheme() {
+   let darkMode = document.getElementById("dark-mode");
+   let theme = document.getElementById("theme");
+   if (theme.getAttribute("href") === lightTheme) {
+      theme.href = darkTheme;
+   } else {
+      theme.href = lightTheme;
+   }
+}
